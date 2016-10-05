3901248eb4afca4d105a84a8b6fc4b9472561580
CarOJ <devil.prowler@gmail.com>
2016-10-05 13:06:18 -0500

    Starter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# MEAN Stack Single Page Application Starter
+# MEAN Stack MQTT Simple Starter
 
-This is a repo for a starter appliation for a Single Page MEAN Stack application. Just download and install and you have a good foundation for building application. 
+This is a repo for a starter application for a Single Page MEAN Stack application. Just download and install and you have a good foundation for building application. 
 
 ## Installation
 1. Download the repository
@@ -10,11 +10,3 @@ This is a repo for a starter appliation for a Single Page MEAN Stack application
 5. View in browser at http://localhost:8080
 
 Use this starter kit to build any MEAN stack application you like.
-
-If you have any questions or requests, email us at [chris@scotch.io](mailto:chris@scotch.io) and we'll keep updating this to make it perfect.
-
-## Future Additions
-- CRUD examples
-- Development and Production Environments
-- Link examples
-- Single Page AngularJS Animations
--- server.js
+++ server.js
@@ -9,6 +9,15 @@ var settings = {
 	port: 1883
 };
 
+var reset = '\u001b[37m';
+var red = '\u001b[31m';
+var green =  '\u001b[32m';
+var yellow = '\u001b[33m';
+var blue = '\u001b[34m';
+var margenta = '\u001b[35m';
+var cyan =  '\u001b[36m';
+//var white =  '\u001b[0m'; reset color
+
 
 var mqttserver = new mosca.Server(settings);
 // configuration ===========================================
@@ -33,19 +42,42 @@ require('./app/routes')(app); // pass our application into our routes
 // start app ===============================================
 app.listen(port);
 mqttserver.attachHttpServer(app);
-console.log('Magic happens on port ' + port); 			// shoutout to the user
+console.log(margenta + 'Magic happens on port: '+ reset + port); 			// shoutout to the user
 
 mqttserver.on('ready',function(){
-		console.log("MQTT server online\n");
-})
+		console.log(green + "MQTT server online puerto: "+ reset + settings.port + "\n");
+});
+
 
 mqttserver.on('clientConnected',function(client){
-		console.log("Cliente conectado con id: "+ client.id +"\n");
-})
+		console.log(blue + "Cliente conectado id: "+ reset + client.id +"\n");
+});
+
+mqttserver.on('clientDisconnected',function(client){
+		console.log(red + "Cliente desconectado id: "+ reset + client.id +"\n");
+		
+		var msg = {
+			topic: client.id + "/status",
+			payload: "Offline",
+			qos: 0,
+			retain: true
+		}
+
+		mqttserver.publish(msg);
+});
+
+mqttserver.on('subscribed',function(topic, client){
+		console.log(yellow + "Cliente id: "+ reset + client.id + yellow+" suscrito a "+ reset + topic +"\n");
+});
 
 
-
-
+mqttserver.on('published',function(packet, client){
+	if(client != undefined){
+		console.log(cyan + "Cliente id: "+ reset + client.id + cyan +
+					" publico a "+ reset + packet.topic + cyan +
+					" contenido: "+ reset + Buffer(packet.payload).toString() +"\n");
+	}
+});
 
 
 
